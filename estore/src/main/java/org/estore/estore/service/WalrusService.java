1446c93c2920381f1fa1162d923aafce758f058b
0d-fako <od.fakorede@gmail.com>
2025-06-02 17:02:18 +0100

    Estore Application

--- estore/src/main/java/org/estore/estore/service/WalrusService.java
+++ estore/src/main/java/org/estore/estore/service/WalrusService.java
@@ -1,9 +1,14 @@
 package org.estore.estore.service;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
-
+@Component
 public class WalrusService {
     public String upload(MultipartFile file) {
-        return null;
+        String walrusUrl = "https://publisher.walrus-testnet.walrus.space/v1/blobs";
+
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.put("walrusUrl", walrusUrl);
     }
 }
