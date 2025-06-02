b112f6bb4e2bec7e06cc97e293f92bf3b8c00b5a
0d-fako <od.fakorede@gmail.com>
2025-06-02 16:38:24 +0100

    Estore Application

--- estore/src/main/java/org/estore/estore/service/WalrusService.java
+++ estore/src/main/java/org/estore/estore/service/WalrusService.java
@@ -1,4 +1,9 @@
 package org.estore.estore.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public class WalrusService {
+    public String upload(MultipartFile file) {
+        return null;
+    }
 }
--- estore/src/test/java/org/estore/estore/service/WalrusServiceTest.java
+++ estore/src/test/java/org/estore/estore/service/WalrusServiceTest.java
@@ -3,6 +3,13 @@ package org.estore.estore.service;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
 
 @SpringBootTest
 public class WalrusServiceTest {
@@ -11,5 +18,18 @@ public class WalrusServiceTest {
 
 
     @Test
+    void testCanUploadFile(){
+        String filelocation = "C:\\Users\\Dell\\Desktop\\PROJECTS\\estore\\estore\\src\\main\\resources\\assets\\iphone.jpg";
+
+        Path path = Paths.get(filelocation);
+
+        try(var inputStream = Files.newInputStream(path)) {
+            MultipartFile file = new MockMultipartFile("image", inputStream);
+            String data = walrusService.upload(file);
+            assert
+        }catch (IOException exception){
+            exception.printStackTrace();
+        }
+    }
 
 }
