4f14f3e62618e84875453cefa997e8d70579df09
0d-fako <od.fakorede@gmail.com>
2025-06-03 12:15:07 +0100

    Estore Application

--- estore/src/main/java/org/estore/estore/service/WalrusService.java
+++ estore/src/main/java/org/estore/estore/service/WalrusService.java
@@ -12,28 +12,33 @@ import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.springframework.http.HttpMethod.PUT;
+
 @Component
 public class WalrusService {
     public String upload(MultipartFile file) {
         String walrusUrl = "https://publisher.walrus-testnet.walrus.space/v1/blobs";
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-        RequestEntity<MultipartFile> requestEntity = new RequestEntity<>(file, headers, HttpMethod.PUT, URI.create(walrusUrl));
+        // RequestEntity<MultipartFile> requestEntity = new RequestEntity<>(file, headers, PUT, URI.create(walrusUrl));
+
         Map<String, Object> params = new HashMap<>();
         params.put("epochs", 5);
         params.put("send_object_to", "0x7a26fe751dbdc0f43e63a51712738241ef7f91e6e9a8a256a958ebd3d565b27e");
 
         Resource resource = file.getResource();
-        HttpEntity<?> httpEntity = new HttpEntity(resource, headers);
+        HttpEntity<?> requestEntity = new HttpEntity<>(resource, headers);
 
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<WalrusUploadResponse> response = restTemplate.exchange(URI.create(walrusUrl), HttpMethod.PUT, requestEntity, WalrusUploadResponse.class);
+        ResponseEntity<WalrusUploadResponse> response = restTemplate.exchange(walrusUrl,PUT, requestEntity, WalrusUploadResponse.class, params);
         WalrusUploadResponse walrusUploadResponse = response.getBody();
-        boolean isFileAlreadyExists = walrusUploadResponse != null && walrusUploadResponse.get
+
+        boolean isFileAlreadyExists = walrusUploadResponse != null && walrusUploadResponse.getNewlyCreated() == null;
 
         if (isFileAlreadyExists) {
-            return walrusUploadResponse.getBlobObject().getBlodId;
+            return walrusUploadResponse.getAlreadyCertified().getBlobId();
         }
+        assert walrusUploadResponse != null;
         return walrusUploadResponse.getNewlyCreated().getBlobObject().getBlodId;
     }
 }
